1e2ce0b8dd3ebe13ffe2a050767724eb1f30873d
Learem <69031001+Learem@users.noreply.github.com>
2020-10-25 11:45:23 +0300

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,20 +1,42 @@
 import sys
 sys.setrecursionlimit(10000)
+reg_st = False
+reg_end = False
 
 
-def consume_regex(word1, word2):
-    if len(word2) < len(word1):
+def reg_metachar(word1, word2, is_slice):
+    if word1[1] == "?":
+        return consume_regex(word1[0] + word1[2:], word2, is_slice) or consume_regex(word1[2:], word2, is_slice)
+    if word1[1] == "+":
+        return (word1[0] == "." or word1[0] == word2[0]) and (consume_regex(word1[2:], word2[1:], is_slice) or consume_regex(word1, word2[1:], is_slice))
+    if word1[1] == "*":
+        return (consume_regex(word1[2:], word2, is_slice)) or ((word1[0] == "." or word1[0] == word2[0]) and (consume_regex(word1[2:], word2[1:], is_slice) or consume_regex(word1, word2[1:], is_slice)))
+
+
+def consume_regex(word1, word2, is_slice):
+    if word1 == "":
+        return reg_st or word2 == "" or not reg_end
+    if word2 == "":
         return False
-    char_1 = word1[0]
-    char_2 = word2[0]
-    is_match = char_1 == "." or char_1 == char_2
-    if len(word1) == 1:
-        return is_match
-    else:
-        if is_match:
-            return consume_regex(word1[1:], word2[1:]) or consume_regex(word1, word2[1:])
+    is_match = word1[0] == "." or word1[0] == word2[0]
+    if word1[0] == "\\":
+        if word1.startswith("\\."):
+            is_match = word1[1] == word2[0]
         else:
-            return consume_regex(word1, word2[1:])
+            is_match = word1[1] == "." or word1[1] == word2[0]
+        word1 = word1[1:]
+    if len(word1) == 1:
+        if reg_end:
+            return (len(word2) == 1 and is_match) or (not reg_st and is_slice and consume_regex(word1, word2[1:], is_slice))
+        elif reg_st:
+            return is_match
+        else:
+            return is_match or (is_slice and consume_regex(word1, word2[1:], is_slice))
+    if word1[1] in "?*+":
+        return reg_metachar(word1, word2, is_slice)
+    if reg_st:
+        return is_match and consume_regex(word1[1:], word2[1:], False)
+    return (is_match and consume_regex(word1[1:], word2[1:], False)) or (is_slice and consume_regex(word1, word2[1:], is_slice))
 
 
 string = input()
@@ -27,4 +49,10 @@ else:
     elif words[1] == "":
         print(False)
     else:
-        print(consume_regex(words[0], words[1]))
+        reg_st = words[0].startswith("^")
+        reg_end = words[0].endswith("$") and not words[0].endswith("\\$")
+        if reg_st:
+            words[0] = words[0][1:]
+        if reg_end:
+            words[0] = words[0][:-1]
+        print(consume_regex(words[0], words[1], True))
